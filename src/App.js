c057a61988380b2ef25ac7f9a0cec96d8a7b6acf
Jaime <11698160+J1Mtonic@users.noreply.github.com>
2023-08-16 16:30:09 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from 'react';
 import Header from "./components/Header";
 import Swap from "./components/Swap";
+import Footer from "./components/Footer";
 import { useConnect, useAccount } from "wagmi";
 import { InjectedConnector } from 'wagmi/connectors/injected';
 
@@ -18,6 +19,7 @@ function App(props) {
       <div className="mainWindow">
         <Swap isConnected={isConnected} address={address} resetBalances={resetBalances} setResetBalances={setResetBalances} networkConfig={props.networkConfig} useTestnet={props.useTestnet} />
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Layout, Typography, Space, Button } from 'antd';
+import { GithubOutlined, TwitterOutlined } from '@ant-design/icons';
+
+const { Footer: AntFooter } = Layout;
+const { Text, Link } = Typography;
+
+function Footer() {
+    return (
+        <AntFooter className="footer-container">
+            <div className="footer-center footer-buttons">
+                <Space size="middle">
+                    <Button icon={<GithubOutlined />} href="https://github.com/J1Mtonic/VAIPSM" target="_blank" />
+                    <Button icon={<TwitterOutlined />} href="https://https://twitter.com/Venus_Community" target="_blank" />
+                </Space>
+            </div>
+            <div className="footer-center">
+                <Space direction="vertical" size="middle">
+                    <Text className="footer-primary">
+                        Made with <Text className="footer-love">❤️</Text> by <Link href="https://venusstars.io" className="gradient-text" target="_blank">VenusStars</Link>
+                    </Text>
+                </Space>
+            </div>
+        </AntFooter>
+    );
+}
+
+export default Footer;
